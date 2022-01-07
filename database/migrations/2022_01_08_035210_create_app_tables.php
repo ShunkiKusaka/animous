0e905f2cf6ff2bbef1e87ce5f5808cba533415d7
ShunkiKusaka <sak20ligings@gmail.com>
2022-01-08 04:53:26 +0900

    <テーブル実装>
    ER図で落とし込んだテーブル設計の沿って、マイグレーションを作成(フォルダ名 :: create_app_tables)
    
    $table->unsignedBigInteger →『対応するカラムのID』を格納するカラムを定義。
    
    $table->foreign('primary_category_id')->references('id')->on('primary_categories')
     → 'primary_categories'テーブル の 'id'カラムと、'secondary_categories'テーブル の 'primary_category_id'カラムを紐付け、'primary_category_id'に、外部キー制約を付与。
    
    <Eloquent Modelの生成>
    
    各テーブルのEloquent ModelをApp/Models/配下に生成
    
    App/配下にあったUser.php(Eloquent Model)をApp/Models/配下に移動。
    それに応じて、nameSpaceがApp/UserだったものをApp/Models/Userに書き換え。

---
+++ database/migrations/2022_01_08_035210_create_app_tables.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()//マイグレーション実行時に呼ばれる
+    {
+        Schema::create('primary_categories', function (Blueprint $table) {
+            $table->id();
+
+            // ここにカラムを追加していく
+
+            $table->timestamps();
+        });
+
+        Schema::create('secondary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('primary_category_id');
+            //primary_categoriesテーブルとsecondary_categoriesテーブルの間の１対多のリレーション
+
+            // ここにカラムを追加していく
+
+            $table->timestamps();
+
+            $table->foreign('primary_category_id')->references('id')->on('primary_categories');
+            //primary_categoriesテーブルとsecondary_categoriesテーブルの間の１対多のリレーション
+        });
+
+        Schema::create('item_conditions', function (Blueprint $table) {
+            $table->id();
+
+            // ここにカラムを追加していく
+
+            $table->timestamps();
+        });
+
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->unsignedBigInteger('buyer_id');
+            $table->unsignedBigInteger('secondary_category_id');
+            $table->unsignedBigInteger('item_condition_id');
+
+            // ここにカラムを追加していく
+
+            $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('users');
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('secondary_category_id')->references('id')->on('secondary_categories');
+            $table->foreign('item_condition_id')->references('id')->on('item_conditions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()//ロールバック時に呼ばれる
+    {
+        Schema::dropIfExists('items');
+        Schema::dropIfExists('item_conditions');
+        Schema::dropIfExists('secondary_categories');
+        Schema::dropIfExists('primary_categories');//primary_categoriesテーブルが存在している場合、削除
+    }
+}
