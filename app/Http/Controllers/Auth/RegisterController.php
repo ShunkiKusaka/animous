adf24e483c005831a2f4bca478fda061a18fdb1e
ShunkiKusaka <sak20ligings@gmail.com>
2022-01-09 04:14:51 +0900

    <バリデーションの日本語化> 日本語バリデーションのjaファイルを作成したら、config/app.phpの"locale"を"ja"に変えるのを忘れずにしましょう。 こうしないと英語のままです。
    <パスワード入力の際のバリデーションルールを変更>
    パスワードは再入力のものも一緒に実装してしまったが、今回は再入力は扱わないので"confirmed"を消した。
    
    <seedr / factoryでテストデータを挿入>
    新たに作ったuserテーブル用のseederは、DatabaseSeeder.phpからcall関数で呼んだ。
    テストデータは"database/seeds/UserSeeder.php"のrunメソッドを参照してください。
    
    <login.blade.phpの画面を独自のものに変更>
    
    <register.blade.php>
    このファイル内のformタグ内のinputにはhtml側からバリデーションできる"required"属性がついてるが、独自のバリデーションを追加したいため消した。
    独自のバリデーションを作りたかったら、"required"は消してください。

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -47,12 +47,12 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
+    protected function validator(array $data)//会員登録のバリデーション変更メソッド
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
     }
 
