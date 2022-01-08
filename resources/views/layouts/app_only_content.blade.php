ee52f08a202a5a7cb5e277ac629ae5d852b990ad
ShunkiKusaka <sak20ligings@gmail.com>
2022-01-09 01:43:14 +0900

    <resources/views/layouts/app_only_content.blade.phpのコメント追加>
    <resources/views/auth/register.blade.phpの実装>

--- resources/views/layouts/app_only_content.blade.php
+++ resources/views/layouts/app_only_content.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100"><!-- 言語設定 -->
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,14 +10,14 @@
     <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script><!-- ファイルのURLを取得して埋め込 -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"><!-- ファイルのURLを取得して埋め込 -->
 
     <link rel="shortcut icon" href="/images/logo.ico">
 </head>
@@ -25,7 +25,7 @@
     <div class="app app-only-content">
         {{-- ロゴ画像 --}}
         <div class="d-flex justify-content-center">
-            <a href="{{route('top')}}"><img src="/images/logo-1.png" /></a>
+            <a href="{{route('top')}}"><img src="/images/logo-1.png" /></a><!-- ロゴ画像を表示 -->
         </div>
         {{-- コンテンツ部分 --}}
         <div class="d-flex justify-content-center mt-3">
