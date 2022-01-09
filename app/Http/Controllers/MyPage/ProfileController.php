bc3d4f74fb88fa237807512c4b6cfcba23eb102e
ShunkiKusaka <sak20ligings@gmail.com>
2022-01-09 19:19:20 +0900

    <プロフィール保存機能> プロフィール編集画面表示機能が完成後、今度はpostで保存できる機能を実装 画像保存機能はこの後やります

--- app/Http/Controllers/MyPage/ProfileController.php
+++ app/Http/Controllers/MyPage/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\MyPage;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\Mypage\Profile\EditRequest;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
@@ -14,5 +15,16 @@ class ProfileController extends Controller
         //userという変数名でログインしているユーザの情報を'mypage.profile_edit_form'に渡している
         //ログインしている場合は、app\Models\Userのインスタンス（usersテーブルのレコード）を返す
         //ログインしていない場合は、nullを返す
+    }
+
+    public function editProfile(EditRequest $request)
+    {
+        $user = Auth::user();//ログインしているユーザの情報を取得
+
+        $user->name = $request->input('name');//取得したいinputタグのname属性の値
+        $user->save();//DBに保存
+
+        return redirect()->back()//直前のページにリダイレクトするレスポンスを生成
+            ->with('status', 'プロフィールを変更しました。');//セッションに値を保存
     }
 }
