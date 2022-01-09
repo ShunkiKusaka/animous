e6e17fecd4db6a79e8069fdffecf66eb7a0d5bff
ShunkiKusaka <sak20ligings@gmail.com>
2022-01-10 03:15:48 +0900

    <全商品一覧画面表示> 表示順は、募集中のものから先で、締め切り済みはその後に表示している

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemsController extends Controller
+{
+    public function showItems(Request $request)
+    {
+        $items = Item::orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )//出品中の商品を先に、購入済みの商品を後に表示
+            ->orderBy('id', 'DESC')//さらにidの降順（最近出品された順）で並べ替え
+            ->get();
+
+        return view('items.items')
+            ->with('items', $items);
+    }
+}
