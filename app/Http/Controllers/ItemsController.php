c3123f7f54132aa0cca9dbe09679dd576372332a
ShunkiKusaka <sak20ligings@gmail.com>
2022-01-10 05:09:39 +0900

    <商品を検索する処理を実装> -------------------------------------------------- <カテゴリーで絞り込み> カテゴリから種別とIDを切り出して、配列の中の別々のindexに入れた
    小カテゴリーについては、商品テーブルの外部キーとして登録しているsecondary_category_idに絞り込みをかけたが、
    大カテゴリーについては、商品テーブルではなく、リレーション先の小カテゴリテーブルにカラムがあるので、小カテゴリテーブルのprimary_category_idに検索をかける形にした。
    
    <キーワードで絞り込み>
    これは、SQLのLIKE句を用いたパターンマッチングを使用
    名前にキーワードが含まれる」または「説明文にキーワードが含まれる」商品を絞り込むためにOR条件に基づいて、orWhere関数を使用
    
    さらに今回は、カテゴリー  ＋　(nameに含まれるキーワード OR 説明に含まれるキーワード)という形にしたかったので、A AND (B OR C)条件を使用
    ORを取り巻く( )はwhereメソッドの第二引数の無名関数（クロージャ）をでOR条件を囲めば実装できた。
    
    最後に、キーワードをエスケープする処理を実装
    %や_で検索をかけると、意図しないパターンマッチングが実行されるので、特殊文字はエスケープするようなメソッドを実装

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -9,12 +9,48 @@ class ItemsController extends Controller
 {
     public function showItems(Request $request)
     {
-        $items = Item::orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )//出品中の商品を先に、購入済みの商品を後に表示
+        $query = Item::query();
+ 
+        // カテゴリで絞り込み
+        if ($request->filled('category')) {//categoryという名前のパラメータが指定されているかどうかを調べる
+            list($categoryType, $categoryID) = explode(':', $request->input('category'));//分割代入
+
+            if ($categoryType === 'primary') {
+                $query->whereHas('secondaryCategory', function ($query) use ($categoryID) {//リレーション先のテーブルのカラムを基に絞り込む
+                    $query->where('primary_category_id', $categoryID);
+                });
+                //無名関数（クロージャ）でリレーション先のテーブルに対する絞り込みを行う
+            } else if ($categoryType === 'secondary') {
+                $query->where('secondary_category_id', $categoryID);
+            }
+            //secandary_category_idはitemテーブルにあるが、primarycategory_idはitemにはなく
+            //リレーション先のsecandary_categoriesの中にあるから、絞り込むときはwhereHas()を使う
+        }
+
+        // キーワードで絞り込み
+        if ($request->filled('keyword')) {
+            $keyword = '%' . $this->escape($request->input('keyword')) . '%';
+            $query->where(function ($query) use ($keyword) {
+                $query->where('name', 'LIKE', $keyword);
+                $query->orWhere('description', 'LIKE', $keyword);
+            });
+        }
+
+        $items = $query->orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )//出品中の商品を先に、購入済みの商品を後に表示
             ->orderBy('id', 'DESC')//さらにidの降順（最近出品された順）で並べ替え
             ->get();
 
         return view('items.items')
             ->with('items', $items);
+    }
+
+    private function escape(string $value)//特殊文字をエスケープ
+    {
+        return str_replace(
+            ['\\', '%', '_'],
+            ['\\\\', '\\%', '\\_'],
+            $value
+        );
     }
 
     public function showItemDetail(Item $item)
